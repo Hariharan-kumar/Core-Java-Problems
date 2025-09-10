3a8687e2782e7421e57573c3b25b9e4fe37fc87f
Hariharan-kumar <hariharank2019@gmail.com>
2025-09-10 15:44:08 +0530

    FibonacciSeries

--- corejava/fibonacci/FibonacciSeries.java
+++ corejava/fibonacci/FibonacciSeries.java
@@ -12,15 +12,19 @@ public class FibonacciSeries {
     }
     public void fibonacciSeries(int target){
         try{
-            int start1 = 0,start2 = 1,sum=0;
-            for(int i =0;i<target;i++){
-                System.out.print(start1 + " ");
-                sum +=start1;
-               int next = start1 + start2;
-                start1 = start2;
-                start2 = next;
+            int start1 = 0,start2 = 1,sum=1;
+            if ( target ==1 || target ==0){
+                System.out.println("Fibonacci Value is : "+ target);
             }
-            System.out.println(" - Fibonacci Value is : " +sum);
+           else {
+                for (int i = 2; i <= target; i++) {
+                    System.out.print(start1 + " ");
+                                       int next = start1 + start2;
+                    start1 = start2;
+                    start2 = next;
+                }
+                System.out.println(" - Fibonacci Value is : " + start2);
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
---
+++ corejava/sumofdigits/SumofDigits.java
@@ -0,0 +1,17 @@
+package com.corejava.sumofdigits;
+
+import java.util.Scanner;
+
+public class SumofDigits {
+
+    Scanner userInput = null;
+
+    public SumofDigits() {
+        userInput = new Scanner(System.in);
+
+    }
+    public void sumOfDigits(){
+
+
+    }
+}
